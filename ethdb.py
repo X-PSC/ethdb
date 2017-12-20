aea8eabba744bc94575d783b0396b52c8099d43c
Achille <achille.nazaret@gmail.com>
2017-12-20 15:31:13 +0100

    Add Filters (source, destination)  when loading db

--- ethdb.py
+++ ethdb.py
@@ -17,11 +17,21 @@ class Transaction():
         return str(self.value) + " from " + self.source + " to " + self.destination
 
 
-def load_db(filename = "../transactions.db", nb=100000):
+def load_db(filename = "../transactions.db", filter_source="", filter_dest="", nb=100000):
     connection = sqlite3.connect(filename)
     cursor = connection.cursor()
 
-    l = cursor.execute("""SELECT id, blockHash, blockNumber, "from", "to", value FROM transactions ORDER BY id DESC LIMIT %d""" % nb).fetchall()
+    filters = "WHERE"
+    if filter_source:
+        filters += '"from" = "%s"' % filter_source    
+    
+    if filter_dest:
+        filters += '"to" = "%s"' % filter_dest
+        
+    if filters == "WHERE":
+        l = cursor.execute("""SELECT id, blockHash, blockNumber, "from", "to", value FROM transactions ORDER BY id DESC LIMIT %d""" % nb).fetchall()
+    else:
+        l = cursor.execute("""SELECT id, blockHash, blockNumber, "from", "to", value FROM transactions %s ORDER BY id DESC LIMIT %d""" % (filters,nb)).fetchall()
     res = []
 
     for t in l:
